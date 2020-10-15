0e55adf98bbfde4ceff69263b4f3bac79fe42c8b
Gogs <gogs@fake.local>
2020-10-15 21:22:45 +0200

    Added tsoa

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+// src/app.ts
+import express from "express";
+import bodyParser from "body-parser";
+import { RegisterRoutes } from "../build/routes";
 
-import express from 'express';
+export const app = express();
 
-const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+// Use body parser to read sent json payloads
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
