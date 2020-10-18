9d8f41ee7d2b302df56412c1f1c9c8d3d6c44594
Gogs <gogs@fake.local>
2020-10-18 13:54:25 +0200

    Add application insights

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { RegisterRoutes } from "../tsoa/routes";
-
+import { setup } from 'applicationinsights'
 export const app = express();
 
 // Use body parser to read sent json payloads
@@ -14,3 +14,9 @@ app.use(
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
+
+const key = process.env["APP_INSIGHTS_KEY"];
+
+if (key) {
+  setup(key).start();
+}
