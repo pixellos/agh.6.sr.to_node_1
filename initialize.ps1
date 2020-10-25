5fd64db7a9af88beb00656baffef1ae67f9b9930
Gogs <gogs@fake.local>
2020-10-25 16:00:03 +0100

    Fix initializer

--- initialize.ps1
+++ initialize.ps1
@@ -3,9 +3,9 @@ docker stop product-microservice:local
 docker stop order-microservice:local
 docker stop frontend-microservice:local
 
-docker build . -f .\product-microservice\Dockerfile-prod -t local
-docker build . -f .\order-microservice\Dockerfile-prod -t local
-docker build . -f .\frontend\Dockerfile-prod -t local
+docker build . -f .\product-microservice\Dockerfile-prod -t product-microservice:local
+docker build . -f .\order-microservice\Dockerfile-prod -t order-microservice:local
+docker build . -f .\frontend\Dockerfile-prod -t frontend-microservice:local
 
 docker run product-microservice:local -p 8081:3000
 docker run order-microservice:local -p 8082:3000
