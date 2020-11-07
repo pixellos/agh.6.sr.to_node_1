ac1bbfc2ab0d62484e8f1fbfe30c97ae71b0463a
gadzik <glina41@o2.pl>
2020-11-07 18:24:52 +0100

    dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,16 +6,25 @@ import { AppComponent } from './app.component';
 import { AdminProductsComponent } from './admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin-orders/admin-orders.component';
 import {mainRoutingProviders, routing} from "../route";
+import { WelcomeComponent } from './welcome/welcome.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule, BrowserAnimationsModule,
     mainRoutingProviders,
     routing
   ],
--- frontend/src/route.ts
+++ frontend/src/route.ts
@@ -1,10 +1,10 @@
 import {RouterModule, Routes} from "@angular/router";
-import {AppComponent} from "./app/app.component";
 import {AdminProductsComponent} from "./app/admin-products/admin-products.component";
 import {AdminOrdersComponent} from "./app/admin-orders/admin-orders.component";
+import {WelcomeComponent} from "./app/welcome/welcome.component";
 export const mainRoutes: Routes = [
     {
-        path: '', component: AppComponent
+        path: '', component: WelcomeComponent
     },
     {
         path: 'admin-products',
