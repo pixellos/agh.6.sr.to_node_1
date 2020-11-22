3295115ef346828a9bfdf1f231a5cd3b84ff583d
pixellos <popiel.mati@gmail.com>
2020-11-22 23:15:01 +0100

    Commoned Dockerfile

--- app-gateway-microservice/src/app.ts
+++ app-gateway-microservice/src/app.ts
@@ -1,8 +1,7 @@
 // src/app.ts
 import express, { Response as ExResponse, Request as ExRequest } from "express";
 import { SetupBase } from "../../commons-microservice/src/app-common";
-import * as swagger from './generated/swagger.json';
 
 export const app = express();
 
-SetupBase(app, swagger);
+SetupBase(app, {});
--- initialize.ps1
+++ initialize.ps1
@@ -4,9 +4,10 @@ docker stop order-microservice
 docker stop frontend-microservice
 docker stop app-gateway-microservice
 
-docker build . -f .\product-microservice\Dockerfile-prod -t product-microservice:local
-docker build . -f .\order-microservice\Dockerfile-prod -t order-microservice:local
-docker build . -f .\app-gateway-microservice\Dockerfile-prod -t app-gateway-microservice:local
+docker build . --build-arg MS_NAME=app-gateway-microservice
+docker build . --build-arg MS_NAME=order-microservice
+docker build . --build-arg MS_NAME=product-microservice
+
 docker build . -f .\frontend\Dockerfile-prod -t frontend-microservice:local
 
 docker run  -p "8091:3000" -d product-microservice:local -n product-microservice
