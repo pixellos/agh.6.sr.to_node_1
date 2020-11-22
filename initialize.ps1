3b9c07b292bf75807f79df52cb51a65c1bf1238b
pixellos <popiel.mati@gmail.com>
2020-11-22 23:25:12 +0100

    Unify dokerfiles

--- initialize.ps1
+++ initialize.ps1
@@ -4,11 +4,11 @@ docker stop order-microservice
 docker stop frontend-microservice
 docker stop app-gateway-microservice
 
-docker build . --build-arg MS_NAME=app-gateway-microservice
-docker build . --build-arg MS_NAME=order-microservice
-docker build . --build-arg MS_NAME=product-microservice
+docker build . --build-arg MS_NAME=app-gateway-microservice -t app-gateway-microservice:local
+docker build . --build-arg MS_NAME=order-microservice -t order-microservice:local
+docker build . --build-arg MS_NAME=product-microservice -t product-microservice:local
 
-docker build . -f .\frontend\Dockerfile-prod -t frontend-microservice:local
+docker build . -f .\frontend\Dockerfile -t frontend-microservice:local
 
 docker run  -p "8091:3000" -d product-microservice:local -n product-microservice
 docker run -p "8092:3000" -d  order-microservice:local -n order-microservice
