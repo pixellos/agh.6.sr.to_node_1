060076e87115c3c1ffa6aed3a6c34e9d67ce1e23
pixellos <popiel.mati@gmail.com>
2020-11-30 20:26:56 +0100

    Ustaw K8S

--- initialize.ps1
+++ initialize.ps1
@@ -4,12 +4,17 @@ docker stop order-microservice
 docker stop frontend-microservice
 docker stop app-gateway-microservice
 
-docker build . --build-arg MS_NAME=app-gateway-microservice -t app-gateway-microservice:local
-docker build . --build-arg MS_NAME=order-microservice -t order-microservice:local
-docker build . --build-arg MS_NAME=product-microservice -t product-microservice:local
-docker build . -f .\frontend\Dockerfile -t frontend-microservice:local
+docker build . --build-arg MS_NAME=app-gateway-microservice -t docker.local:5000/app-gateway-microservice
+docker build . --build-arg MS_NAME=order-microservice -t docker.local:5000/order-microservice
+docker build . --build-arg MS_NAME=product-microservice -t docker.local:5000/product-microservice
+docker build . -f .\frontend\Dockerfile -t docker.local:5000/frontend-microservice
 
-docker run  -p "8091:3000" -d product-microservice:local -n product-microservice
-docker run -p "8092:3000" -d  order-microservice:local -n order-microservice
-docker run -p "8095:3000" -d  app-gateway-microservice:local -n app-gateway-microservice
-docker run  -p "809:80" -d frontend-microservice:local -n frontend-microservice
+docker push localhost:5000/product-microservice
+docker push localhost:5000/order-microservice
+docker push localhost:5000/frontend-microservice
+docker push localhost:5000/app-gateway-microservice
+
+# docker run  -p "8091:3000" -d product-microservice:local -n product-microservice
+# docker run -p "8092:3000" -d  order-microservice:local -n order-microservice
+# docker run -p "8095:3000" -d  app-gateway-microservice:local -n app-gateway-microservice
+# docker run  -p "809:80" -d frontend-microservice:local -n frontend-microservice
