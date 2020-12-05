d037401345e35ceac7d8726b2e061a8c9fc2094d
pixellos <popiel.mati@gmail.com>
2020-12-05 19:17:30 +0100

    Konteneryzacja pozostałych serwisów

--- initialize.ps1
+++ initialize.ps1
@@ -1,20 +1,21 @@
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 docker stop product-microservice
 docker stop order-microservice
 docker stop frontend-microservice
 docker stop app-gateway-microservice
 
-docker build . --build-arg MS_NAME=app-gateway-microservice -t docker.local:5000/app-gateway-microservice
-docker build . --build-arg MS_NAME=order-microservice -t docker.local:5000/order-microservice
-docker build . --build-arg MS_NAME=product-microservice -t docker.local:5000/product-microservice
-docker build . -f .\frontend\Dockerfile -t docker.local:5000/frontend-microservice
+docker build . --build-arg MS_NAME=app-gateway-microservice -t docker.local:5000/app-gateway-microservice:latest
+docker build . --build-arg MS_NAME=order-microservice -t docker.local:5000/order-microservice:latest
+docker build . --build-arg MS_NAME=product-microservice -t docker.local:5000/product-microservice:latest
+docker build . -f .\frontend\Dockerfile -t docker.local:5000/frontend-microservice:latest
 
-docker push localhost:5000/product-microservice
-docker push localhost:5000/order-microservice
-docker push localhost:5000/frontend-microservice
-docker push localhost:5000/app-gateway-microservice
+docker push docker.local:5000/product-microservice
+docker push docker.local:5000/order-microservice
+docker push docker.local:5000/frontend-microservice
+docker push docker.local:5000/app-gateway-microservice
 
 # docker run  -p "8091:3000" -d product-microservice:local -n product-microservice
-# docker run -p "8092:3000" -d  order-microservice:local -n order-microservice
+docker run -p "8092:3000" docker.local:5000/order-microservice:latest
 # docker run -p "8095:3000" -d  app-gateway-microservice:local -n app-gateway-microservice
 # docker run  -p "809:80" -d frontend-microservice:local -n frontend-microservice
--- pods/robto.ps1
+++ pods/robto.ps1
@@ -1,11 +1,24 @@
-kubectl delete pod frontend-microservice --namespace=aghlegro
-kubectl delete service frontend-microservice-service --namespace=aghlegro
+$services = @("frontend", "order", "product", "app-gateway")
+
+foreach ($svc in $services) {
+    kubectl delete pod "$svc-microservice" --namespace=aghlegro
+}
+
+foreach ($svc in $services) {
+    kubectl delete service "$svc-microservice-service" --namespace=aghlegro
+}
+
+kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml --namespace=default
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 
 kubectl apply -f .\service.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 kubectl apply -f .\namespace.yaml
-kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml --namespace=default
-kubectl apply -f .\frontend-microservice.yaml --namespace=aghlegro
+
+foreach ($svc in $services) {
+    kubectl apply -f ".\$svc-microservice.yaml" --namespace=aghlegro
+}
+
+write-host "Set up successfull - now app will wait for request"
 
 kubectl proxy
 # http://localhost:8001/api/v1/namespaces/aghlegro/services/frontend-microservice-service/proxy/
