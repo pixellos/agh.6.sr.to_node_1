a2a71bf869ec60c80a69d9e81666ecafbf8ae6ab
pixellos <popiel.mati@gmail.com>
2020-12-30 18:26:30 +0100

    Changed to id

--- order-microservice/src/Orders.ts
+++ order-microservice/src/Orders.ts
@@ -40,7 +40,7 @@ export namespace Orders {
     })
     if (createResult === undefined)
       return errorResponse({ message: 'Order cannot be saved. Try again.' });
-    return okResponse(previousRevision + 1);
+    return okResponse(createResult.id);
   }
 
   export async function SendOrderCommand(props: { id: string, type: 'SendOrderCommand', user: string }) {
