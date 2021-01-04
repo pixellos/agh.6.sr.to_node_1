19e09389e71b382fc58ad8ed4a4faae355bd905c
pixellos <popiel.mati@gmail.com>
2021-01-04 20:46:11 +0100

    Bring back AI for microservices

--- app-gateway-microservice/src/server.ts
+++ app-gateway-microservice/src/server.ts
@@ -1,5 +1,19 @@
 const path = require('path');
 const gateway = require('express-gateway');
+import * as ai from 'applicationinsights';
+
+ai.setup()
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(false)
+    .setDistributedTracingMode(ai.DistributedTracingModes.AI)
+    .start();
+  
 
 gateway()
   .load(path.join(__dirname, 'config'))
