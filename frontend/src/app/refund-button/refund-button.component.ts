352d908f57ccafe300729261b8887881feb442a2
gadzik <glina41@o2.pl>
2021-01-23 19:15:08 +0100

    admin panel part 3

--- frontend/src/app/refund-button/refund-button.component.ts
+++ frontend/src/app/refund-button/refund-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ICellRendererAngularComp} from "ag-grid-angular";
+import {DefaultService as OrderHttpClient} from "../../client-order";
 
 @Component({
   selector: 'app-refund-button',
@@ -9,10 +10,14 @@ import {ICellRendererAngularComp} from "ag-grid-angular";
 export class RefundButtonComponent implements ICellRendererAngularComp {
   public params: any;
 
-  constructor() { }
+  constructor(private httpClient: OrderHttpClient) { }
 
   agInit(params: any): void {
     this.params = params;
+  }
+
+  refund() {
+    this.httpClient.acceptRefund("invalid product", this.params.data.id).subscribe(response => {})
   }
 
   refresh(): boolean {
--- frontend/src/app/send-button/send-button.component.ts
+++ frontend/src/app/send-button/send-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ICellRendererAngularComp} from "ag-grid-angular";
+import {DefaultService as OrderHttpClient} from "../../client-order";
 
 @Component({
   selector: 'app-send-button',
@@ -9,12 +10,15 @@ import {ICellRendererAngularComp} from "ag-grid-angular";
 export class SendButtonComponent implements ICellRendererAngularComp {
   public params: any;
 
-  constructor() { }
+  constructor(private httpClient: OrderHttpClient) { }
 
   agInit(params: any): void {
     this.params = params;
   }
 
+  send() {
+    this.httpClient.send({"id": this.params.data.id}).subscribe(response => {})
+  }
 
   refresh(): boolean {
     return false;
